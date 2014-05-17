3ea7700fae04eb8047384872dc962d2881d8af85
Capster <m6277@yandex.ru>
2014-05-17 21:13:57 +0400

    [VFS] Added Buffer Object types

--- lua/gpad/vfs/bufferobject.lua
+++ lua/gpad/vfs/bufferobject.lua
@@ -29,6 +29,51 @@ function META:RemoveElement(numID)
 	table.remove(self.Buffer, numID)
 end
 
+-- Push
+
+function META:PushString(strString)
+	self:AddElement(strString)
+end
+
+function META:PushNumber(numNumber)
+	self:AddElement(numNumber)
+end
+
+function META:PushTable(tblTable)
+	self:AddElement(tblTable)
+end
+
+function META:PushBool(boolBool)
+	self:AddElement(boolBool)
+end
+
+-- Pull
+
+function META:PullNextByType(strType)
+	for curIndex, curElement in pairs(self:GetBuffer()) do
+		if type(curElement) == strType then
+			return curElement, table.remove(self:GetBuffer(), curIndex)
+		end
+	end
+end
+
+function META:PullString()
+	return self:PullNextByType("string")
+end
+
+function META:PullNumber()
+	return self:PullNextByType("number")
+end
+
+function META:PullTable()
+	return self:PullNextByType("table")
+end
+
+function META:PullBool()
+	return self:PullNextByType("boolean")
+end
+
+
 function GPad.VFS.Protocol.MakeBuffer(tblBuffer)
 	local inBuffer = setmetatable({}, META)
 	inBuffer:SetBuffer(tblBuffer)
