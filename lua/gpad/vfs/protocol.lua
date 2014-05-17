1a43dbea5be50d252138ae656999a3a7cfe36f99
Capster <m6277@yandex.ru>
2014-05-17 20:39:34 +0400

    [VFS] Added Protocol Send and Unregister functions

--- lua/gpad/vfs/protocol.lua
+++ lua/gpad/vfs/protocol.lua
@@ -1,10 +1,46 @@
 GPad.VFS.Protocol = {}
+GPad.VFS.Protocol.Channels = {}
 
 function GPad.VFS.Protocol:RegisterChannel(strName)
 	if SERVER then
 		GPad.VFS.Debug("Protocol:RegisterChannel() -> "..strName)
 		util.AddNetworkString(strName)
+		GPad.VFS.Protocol.Channels[strName] = true
 	else
 		GPad.VFS.Error("Unhandled channel register!")
 	end
+end
+
+function GPad.VFS.Protocol:UnregisterChannel(strName)
+	if SERVER then
+		GPad.VFS.Protocol.Channels[strName] = false
+	else
+		GPad.VFS.Error("Unhandled channel unregister!")
+	end
+end
+
+function GPad.VFS.Protocol:Receive(strName, funcCallback, entPlayer)
+	if SERVER and not GPad.VFS.Protocol.Channels[strName] then
+		GPad.VFS.Error("unpooled channel name: "..strName)
+		return false
+	end
+	net.Start(strName)
+	if CLIENT then
+		return net.SendToServer()
+	else
+		if entPlayer then
+			return net.Send(entPlayer)
+		else
+			return net.Broadcast()
+		end
+	end
+	
+end
+
+function GPad.VFS.Protocol:Send(strName, ...)
+	net.Receive(strName, function(numLength, entPlayer)
+		inBuffer = {}
+		funcCallback(inBuffer, numLength, entPlayer)
+	end)
+	
 end
