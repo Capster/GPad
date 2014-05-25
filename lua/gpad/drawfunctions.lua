79a7a5b85f8c19f4750042765d9ffb572dfba4d5
Lena <SkyAngeLoL@yandex.ru>
2014-05-25 11:03:31 -0700

    [UI] Fixed VS2013 Dot function
    ^___________^

--- lua/gpad/drawfunctions.lua
+++ lua/gpad/drawfunctions.lua
@@ -3,11 +3,10 @@ local cos, sin, rad, DrawPoly, SetTexture = math.cos, math.sin, math.rad, surfac
 
 function surface.DrawVSDotHorizontal(x, y, len, color)
 	setcolor(color or color_white)
-		for i = 0, len do
-			local add = mod(i, 4) ~= 0 and 1 or 0
-		
-			rect(x + i + add, y - 2, 1, 1)
-			rect(x + i + add + 2, y, 1, 1)
-			rect(x + i + add, y + 2, 1, 1)
+		for i = 0, (len / 4 - 1) do
+			local k = i * 4
+			rect(x + k, y - 2, 1, 1)
+			rect(x + k + 2, y, 1, 1)
+			rect(x + k, y + 2, 1, 1)
 		end
 end
