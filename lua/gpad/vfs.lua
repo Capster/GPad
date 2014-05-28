9c88d82429771938b98ead89fab087dcb5903756
Capster <m6277@yandex.ru>
2014-05-28 21:10:19 +0400

    [VFS] Added print function

--- lua/gpad/vfs.lua
+++ lua/gpad/vfs.lua
@@ -1,3 +1,10 @@
+GPad.VFS.Color = Color(0xA2, 0xA2, 0xFF)	
+
+function GPad.VFS.Print(...)
+	MsgC(GPad.VFS.Color, "[VFS] ") print(...)
+end
+
+
 function GPad.VFS.Debug(...)
 	if GPad.DEVMODE then
 		Msg"[VFS] " print(...)
