de975c61c505358e2281e470ce5830372682896a
Capster <m6277@yandex.ru>
2014-05-29 23:56:53 +0400

    [Execute] Added Revert hook

--- lua/gpad/compilers/breakpoint.lua
+++ lua/gpad/compilers/breakpoint.lua
@@ -2,3 +2,8 @@ function GPad:BreakPoint(numLine)
 	hook.Run("GPad.BreakpointCall")
 	GPad.PrintDebug(nil, "Exec Breakpoint on line: "..numLine)
 end
+
+function GPad:RevertPoint(numLine)
+	hook.Run("GPad.RevertPoint")
+	-- ToDO: ...
+end
