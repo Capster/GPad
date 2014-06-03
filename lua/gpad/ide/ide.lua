1a7ad7749f580e2b987e7a931e113cc20a0d335d
Capster <m6277@yandex.ru>
2014-06-04 01:47:42 +0400

    [UI] Added Sessions

--- lua/gpad/ide/ide.lua
+++ lua/gpad/ide/ide.lua
@@ -1,3 +1,5 @@
+GPad.Docks = {}
+
 function GPad:GetFrame()
 	GPad.Panel = Metro.Create("GPadIDE")
 	GPad.Panel:SetVisible(false)
@@ -6,4 +8,18 @@ end
 
 concommand.Add ("gpad_show",function ()
 	GPad:GetFrame():SetVisible (true)
+end)
+
+function GPad:SaveSession()
+	local tblDump = table.ClearKeys(GPad.Docks)
+	luadata.WriteFile("gpad_workspace.txt", tblDump)
+end
+
+function GPad:LoadSession()
+	GPad.PrintTable(luadata.ReadFile("gpad_workspace.txt"))
+	return luadata.ReadFile("gpad_workspace.txt")
+end
+
+timer.Create("GPad.AutoSave", 5, 0, function()
+	GPad:SaveSession()
 end)
