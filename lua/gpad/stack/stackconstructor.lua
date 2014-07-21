b3bab5c1e7b5b26474ae803738ccb597464eb6e1
Capster <m6277@yandex.ru>
2014-07-21 20:51:19 +0400

    Removed !Cake's code (Stack)

--- lua/gpad/stack/stackconstructor.lua
+++ lua/gpad/stack/stackconstructor.lua
@@ -3,52 +3,27 @@ GPad.Stack = GPad.Consturctor()
 local META = {}
 META.__index = META
 
-META.Type = "stack"
-
 META.Elements 	= {}
-META.TopElement = nil
-META.Count		= 0
-
-function META:__tostring()
-	local stack = "[Stack ("..self.Count..")]"
-	for i = 1, self.Count do
-		stack = stack.."\n["..tostring(i).."] "
-		if type(self.Elements[i]) == "table" and type(self.Elements[i].ToString) == "function" then
-			stack = stack..self.Elements[i]:ToString()
-		else
-			stack = stack..tostring(self.Elements[i])
-		end
-	end
-	return stack
-end
 
 function META:__len()
-	return #self.Count
+	return #self.Elements
 end
 
-function META:Push(variable)
-	self.Count = self.Count + 1
-	self.TopElement = variable
-	self.Elements[self.Count] = variable
+function META:Push(...)
+	local args = {...}
+	for k, v in ipairs(args) do
+		table.insert(self.Elements, v)
+	end
 end
 
 function META:Pop()
-	if self:IsEmpity() then return nil end
-	local TopElement = self.TopElement
-	self.Elements[self.Count] = nil
-	self.Count = self.Count - 1
-	self.TopElement = self.Elements[self.Count]
-	return TopElement
+	if #self == 0 then return nil end
+	self.Elements[#self.Elements] = nil
+	return self.Elements[#self.Elements]
 end
 
-function META:Peek(offset)
-	offset = offset or 0
-	if offset < 0 then offset = -offset end
-	return self.Items[self.Count - offset]
-end
-
-function META:IsEmpty()
-	return self.Count == 0
+function META:List()
+	return self.Elements
 end
 
 function GPad.Stack.MakeConstructor()
